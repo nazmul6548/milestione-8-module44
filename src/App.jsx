b8cb7ab4dfce118bcaa4eeb3eb582211dc8b5fc6
nazmul jewel <nazmul.islam.juwel9@gmail.com>
2024-03-18 21:58:18 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <h1>nazmul islam jewel</h1>
+      <ul className="steps">
+  <li className="step step-primary">Register</li>
+  <li className="step step-primary">Choose plan</li>
+  <li className="step">Purchase</li>
+  <li className="step">Receive Product</li>
+</ul>
+    </>
+  )
+}
+
+export default App
